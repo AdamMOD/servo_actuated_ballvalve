91fb57a75e9d405a56df032804bef55e7f999495
Adam <adamthemodguy@gmail.com>
2021-08-29 22:54:06 -0700

    got MIP reading with super janky connector

--- main/main.ino
+++ main/main.ino
@@ -14,6 +14,9 @@ enum valve{regulator_1=1};
 #define SERVO_FREQ 330
 #define REG1_US_CLOSED 1800
 #define REG1_US_OPEN 900
+#define MIN_PSIG 0.0
+#define MAX_PSIG 100.0
+#define SUPPLY_VOLTAGE 5.0
 
 void commandServo(float open_fraction, int valve)
 {
@@ -34,6 +37,10 @@ void commandServo(float open_fraction, int valve)
   }
 }
 
+float computePSI(float voltage)
+{
+  return (voltage - .1 * SUPPLY_VOLTAGE ) * (MAX_PSIG - MIN_PSIG) / (.8 * SUPPLY_VOLTAGE) + MIN_PSIG;
+}
 /*
 float readADCDegrees(int joint)
 {
@@ -57,7 +64,7 @@ template <typename T> int sgn(T val) {
 
 void setup() {
   Serial.begin(9600);
-  Serial.print("Time,Command,Reading\n");
+  Serial.print("Time,Command,Reading,Reading\n");
   
   // The ADC input range (or gain) can be changed via the following
   // functions, but be careful never to exceed VDD +0.3V max, or to
@@ -72,9 +79,12 @@ void setup() {
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
   
-  ads.begin();
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }
   ads.setGain(GAIN_TWOTHIRDS);
-  ads.setDataRate(RATE_ADS1115_860SPS);
+  //ads.setDataRate(RATE_ADS1115_860SPS);
 
   
   pwm.begin();
@@ -84,31 +94,35 @@ void setup() {
 }
 
 void loop() {
-  
+  int16_t adc0;
   int16_t adc;
   float  volts;
   float time_seconds;
   float command_us;
+  float psi;
   float command;
-  volts = ads.computeVolts(0);
-
+  adc0 = ads.readADC_SingleEnded(0);
+  volts = ads.computeVolts(adc0);
+  psi = computePSI(volts);
 
   time_seconds = ((float) millis()) / 1000.0 - time_zero_seconds;
   // sin command
-  command_us = 1350 + 45 * sin(time_seconds);
+  //command_us = 1350 + 450 * sin(time_seconds);
   //square command
   //command_us = 1333 + 400 * sgn(sin(time_seconds));
-  command = .5; //+ .5 * sin(time_seconds);
+  command = .5; + .5 * sin(time_seconds);
   //adc = ads.readADC_SingleEnded(1);
   //pwm.writeMicroseconds(1, command_us);
   commandServo(command, regulator_1);
   //pwm.writeMicroseconds(1, command_us);
   
-  Serial.print(time_seconds, 4); Serial.print(",");
+  Serial.print(time_seconds, 4); Serial.print("s, ");
   //Serial.print(command_us); Serial.print(","); 
   //Serial.print(adc); Serial.print("\n");
-  Serial.print(command); Serial.print(",");
-  Serial.print("  "); Serial.print(volts); Serial.println("V");
+  Serial.print(command, 4); Serial.print("cmd, ");
+  Serial.print(volts, 4); Serial.print("V, ");
+  Serial.print(psi, 4); Serial.println("psi, ");
+  
   
   delay(10);
   
